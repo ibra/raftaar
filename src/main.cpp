6521291541b687ecc987d9f66b8907ce31a04dd4
ibra <muhammadibrahimhisham@gmail.com>
2025-11-05 10:12:46 +0500

    add states for stats and quitting

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,18 @@
 
 using namespace ftxui;
 
-void run_stats_page(float value) {
+enum GameMode {
+  STARTING,
+  MODE_WORDS,
+  MODE_TIMED,
+  MODE_EQUATION,
+  STATS,
+  QUITTING
+};
+
+void run_stats_page() {
   sf::RenderWindow window(sf::VideoMode(1280, 720), "raftaar - stats");
+
   sf::Font font;
   if (!font.loadFromFile("assets/arial.ttf")) {
     printf("error: font file not found\n");
@@ -22,48 +32,54 @@ void run_stats_page(float value) {
         window.close();
     }
 
-    text.setString("value: " + std::to_string(value));
     window.clear();
     window.draw(text);
     window.display();
   }
 }
 
+void run_typing_test(GameMode mode) {}
+
 int main() {
   ScreenInteractive screen = ScreenInteractive::TerminalOutput();
-
-  float value = 0.0f;
-  bool quit = false;
+  GameMode mode = GameMode::STARTING;
   bool open_stats_page = false;
 
-  auto increment_button = Button("Increment", [&] { value += 1.0f; });
-  auto dashboard_button = Button("Open Dashboard", [&] {
-    open_stats_page = true;
-    screen.ExitLoopClosure()();
-  });
-  auto quit_button = Button("Quit", [&] {
-    quit = true;
-    screen.ExitLoopClosure()();
-  });
+  if (mode == GameMode::STARTING) {
+    auto words_mode_button =
+        Button("Words Mode", [&] { run_typing_test(GameMode::MODE_WORDS); });
+    auto timed_mode_button =
+        Button("Timed Mode", [&] { run_typing_test(GameMode::MODE_TIMED); });
+    auto equation_mode_button = Button(
+        "Equation Mode", [&] { run_typing_test(GameMode::MODE_EQUATION); });
 
-  auto container =
-      Container::Vertical({increment_button, dashboard_button, quit_button});
+    auto dashboard_button = Button("Open Dashboard", [&] {
+      open_stats_page = true;
+      screen.ExitLoopClosure()();
+    });
 
-  auto ui = Renderer(container, [&] {
-    return vbox({
-               text("raftaar - main menu"),
-               separator(),
-               text("value: " + std::to_string(value)),
-               separator(),
-               container->Render(),
-           }) |
-           center | border;
-  });
+    auto quit_button = Button("Quit", [&] {
+      mode = GameMode::QUITTING;
+      screen.ExitLoopClosure()();
+    });
 
-  screen.Loop(ui);
+    auto container = Container::Vertical(
+        {words_mode_button, timed_mode_button, equation_mode_button});
 
-  if (open_stats_page && !quit)
-    run_stats_page(value);
+    auto ui = Renderer(container, [&] {
+      return vbox({
+                 text("raftaar - main menu"),
+                 separator(),
+                 separator(),
+                 container->Render(),
+             }) |
+             center | border;
+    });
 
-  return 0;
+    screen.Loop(ui);
+  } else if (mode == GameMode::STATS) {
+    run_stats_page();
+  }
+
+    return 0;
 }
