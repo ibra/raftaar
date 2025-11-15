0608326837231c5891de787c2697f869fb851cd3
ibrahim hisham <muhammadibrahimhisham@gmail.com>
2025-11-15 13:38:34 +0500

    move equation generation function to separate file

---
+++ src/equation_generation.hpp
@@ -0,0 +1,4 @@
+#include <vector>
+#include <string>
+
+std::vector<std::string> generate_equations(int count);
--- src/mode_equations.cpp
+++ src/mode_equations.cpp
@@ -1,71 +1,13 @@
 #include "mode_utils.hpp"
 #include <cstdlib>
 #include <ctime>
+#include <equation_generation.hpp>
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <string>
 #include <vector>
 using namespace ftxui;
-
-std::vector<std::string> generate_equations(int count)
-{
-  std::vector<std::string> equations;
-  srand(time(0));
-
-  for (int i = 0; i < count; i++)
-  {
-    int type = rand() % 10;
-
-    // generating a bunch of random constants here that can be plugged into the equations
-    int a = (rand() % 48) + 1;
-    int b = (rand() % 48) + 1;
-    int c = (rand() % 20) + 1;
-    int n = (rand() % 4) + 2;
-
-    std::string eq;
-    switch (type)
-    {
-    case 0:
-      eq = "x^2+" + std::to_string(b) + "x+" + std::to_string(c) + "=0";
-      break;
-    case 1:
-      eq = std::to_string(a) + "x^2-" + std::to_string(b) +
-           "x=" + std::to_string(c);
-      break;
-    case 2:
-      eq = "x^" + std::to_string(n) + "";
-      break;
-    case 3:
-      eq =
-          "dy/dx = " + std::to_string(a) + "x^" + std::to_string(n);
-      break;
-    case 4:
-      eq = "lim_{x\\to0}\\frac{\\sin(x)}{x}";
-      break;
-    case 5:
-      eq = std::to_string(a) + "x+" + std::to_string(b) +
-           "y=" + std::to_string(c);
-      break;
-    case 6:
-      eq = "sqrt{" + std::to_string(a * a) + "}+" + std::to_string(b);
-      break;
-    case 7:
-      eq = "e^{" + std::to_string(a) + "x}=" + std::to_string(c);
-      break;
-    case 8:
-      eq = "log_{" + std::to_string(a) + "}(" + std::to_string(c) + ")";
-      break;
-    case 9:
-      eq = std::to_string(a) + "sin(x)+" + std::to_string(b) + "cos(x)";
-      break;
-    }
-
-    equations.push_back(eq);
-  }
-
-  return equations;
-}
 
 void run_equations_mode()
 {
